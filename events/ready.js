00739f0999effff14c1d409d33a14ccbbbd4f111
Rolex <87032497+Rolexjs@users.noreply.github.com>
2021-11-30 18:54:08 +0300

    Delete events directory

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client) => {
-  client.user.setPresence({ activity: { name: client.config.botStatus }, status: "dnd" });
-  console.log("Bot hazır!");
-};
-
-module.exports.config = {
-  name: "ready"
-};
